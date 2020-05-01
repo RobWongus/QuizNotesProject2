fb1f3aca65277ce0bc1e33eb1fe6251c72f3457b
Alex Taylor <altays633@gmail.com>
2020-05-01 16:26:25 -0400

    creating a test route for create page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,4 +28,10 @@ module.exports = function(app) {
     res.render(path.join(__dirname, "../views/members"), {});
   });
 
+  app.get("/create", function(req, res) {
+    // If the user already has an account send them to the members page
+    
+    res.render(path.join(__dirname, "../views/create"), {});
+  });
+
 };
