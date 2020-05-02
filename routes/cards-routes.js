0c4a77f3cb96d59d2e15751c40c16e56d985c307
Nathan Long <nrlong13@gmail.com>
2020-05-02 12:45:57 -0400

    adding correct route for cards.

---
+++ routes/cards-routes.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+
+module.exports = function (app){
+
+    app.get("/api/cards", function(req, res){
+        let query = {};
+        if(req.query.category_id){
+            query.CategoryId = req.query.categories_id;
+        }
+
+        db.Cards.findAll({
+            where: query,
+            include: [db.Categories]
+        }).then(function(dbCards){
+            res.json(dbCards)
+        });
+    });
+
+    app.get("/api/cards/:id", function(req, res){
+        db.Cards.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Categories]
+        }).then(function(dbCards){
+            res.json(dbCards);
+        });
+    });
+
+    app.post("/api/cards", function(req, res){
+        db.Cards.create({
+            name: req.body.name
+        }).then(function(dbCards){
+            res.json(dbCards)
+        });
+    });
+
+};
