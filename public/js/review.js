d61a5fdeffac8824025b20a813c595a35419e62c
Alex Taylor <altays633@gmail.com>
2020-05-04 23:33:26 -0400

    corrected code for viewing all cards and developing code for selecting cards from a specific category

--- public/js/review.js
+++ public/js/review.js
@@ -2,20 +2,17 @@ $(document).ready(function(){
   // new category variables
   let categoryId;
   let categorySelect = $("#category");
-  let selectedOption;
+  let selectedOption; 
   let card123123 = $("#cards")
 
-  // on submit for category form, run newCategorySUbmit
-  // $(document).on("submit", "#category-form", newCategorySubmit);
-  
   getCategories();
-  // getCards();
-
-  // createCard();
-
+  getAllCards();
+  
   // calling categories for dropdowns
   function getCategories(){
       $.get("/api/categories", renderCategories)
+      // render all
+      getCards();
   }
 
   //talks to db
@@ -25,10 +22,6 @@ $(document).ready(function(){
           window.location.href = "/create"
       }
 
-      // otherwise, create an empty array and then push the categories to it
-      // then, empty the category select element
-      // then, append the rows to add
-      // then, set the value of those options to the categoryId
 
       let rowsToAdd = [];
       for(let i = 0; i < data.length; i++){
@@ -41,11 +34,6 @@ $(document).ready(function(){
 
   //creates category rows
   function createCategoryRow(category){
-    // create an option
-    // target the value, set that to the id of the category
-    // set the text to the name of the category
-    // return the option
-
     let options = $("<option>");
     options.attr("value", category.id);
     options.text(category.name);
@@ -53,40 +41,72 @@ $(document).ready(function(){
   }
 
   // runs getCards api call
-  function getCards(category){
-    $.get("/api/cards/:id", renderCards);
+  function getAllCards(){
+    $.get("/api/cards/", renderAllCards);
+    console.log("getting all categories")
   }
 
   // get selected Category when there is a change in category
-  $("select.form-control").change(function(){
-    selectedOption = $(this).children("option:selected").val();
-    console.log(selectedOption);
-  });
-  
-  //talk to dB to get cards from the database with the specific category
-  function renderCards(data){
-    // what does this do?
-    if(!data.length){
-        window.location.href = "/review"
-    }
 
-    // otherwise, create an empty object and then loop through the cards, then push all of the cards from the dB to that
-    // then, empty the category select element
-    // then, append the cards
-    // then, set the value of those options to the question, answer, and category ID
-    console.log(selectedOption)
-    createCard();
+  function getCards() {
+    $("select.form-control").change(function(){
+      selectedOption = $(this).children("option:selected").val();
+      let selectCardRoute = "/api/cards/";
+      let getCardRoute = selectCardRoute + selectedOption;
+      // console.log(getCardRoute);
+      
+      // issue is that an object is being returned, when it should be in the form of an array
+      // need to turn convert returned data into an array?
+      $.get(getCardRoute).then( function(result) {
+        // console.log(result);
+        // console.log(Object.values(result));
+        renderSpecificCards(result);
+      })
+    })
+  };
+  
+  
+  //talk to dB to get cards from the database to get All Cards
+  function renderAllCards(data){
+    console.log(data);
+    console.log(data.length);
+    let cardsToAdd = [];
+    for(let i = 0; i < data.length; i++){
+        cardsToAdd.push(createCard(data[i]));
+        console.log(createCard(data[i]));
+        // console.log(typeof data + " " + data.length);
+    }
+    console.log(cardsToAdd)
+    card123123.empty();
+    card123123.append(cardsToAdd);
+    card123123.val(cardsToAdd);
+    // console.log("adding cards")
+  }
+
+  function renderSpecificCards(data){
+    console.log(data);
+    console.log(data.length);
+
+    // lenght is coming back undefined
+    // issue is that there is no length coming back?
+    
+    let cardsToAdd = [];
+    for(let i = 0; i < data.length; i++){
+      cardsToAdd.push(createCard(data[i]));
+      console.log(createCard(data[i]));
+      // console.log(card);
+    }
+    // array is coming back empty
+    console.log(cardsToAdd)
+    card123123.empty();
+    card123123.append(cardsToAdd);
+    card123123.val(cardsToAdd);
+    // console.log("adding cards")
+
   }
 
   //creates category rows
-  function createCard(){
-    // create a series of nested divs
-      // col div
-      // card div
-      // card body div
-      // h5 element
-      // p element
-      // button element
+  function createCard(data){
    
     let colDiv = $("<div>");
     let cardDiv = $("<div>");
@@ -95,29 +115,42 @@ $(document).ready(function(){
     let cardBodyPara = $("<p>")
     let cardBodyButton = $("<button>");
 
-    // from create
-    let options = $("<option>");
-    options.attr("value", category.id);
-    options.text(category.name);
 
     // add classes, text
-      //col div - add col class and sizing
-      //card div - add card body class
-      //h5 - add card-title class, name text from dB
-      // p - add card-text, question text from db
-      // button - add button type, class, data toggle, data-target, data-type, and text
-    
+    colDiv.addClass("col-3 m-2");
+    cardDiv.addClass("card");
+    cardBodyDiv.addClass("card-body");
+    cardBodyHeading.addClass("card-title");
+    cardBodyPara.addClass("card-text");
+    cardBodyButton.attr("type","button");
+    cardBodyButton.attr("class","btn btn-primary");
+    cardBodyButton.attr("data-toggle","modal");
+    cardBodyButton.attr("data-target","#exampleModal");
+    cardBodyButton.attr("data-whatever","expand1");
+
+    // add classes and params
+
+    //col div - add col class and sizing
+    //card div - add card body class
+    //h5 - add card-title class, name text from dB
+    // p - add card-text, question text from db
+    // button - add button type, class, data toggle, data-target, data-type, and text
+    cardBodyHeading.text(data.question);
+    cardBodyPara.text(data.answer);
+    cardBodyButton.text("Expand");
+    // add text from DB
+      
+      
     // append divs and elements together
 
     colDiv.append(cardDiv);
     cardDiv.append(cardBodyDiv);
     cardBodyDiv.append(cardBodyHeading);
     cardBodyDiv.append(cardBodyPara);
-    cardBodyDiv.append(cardBodyButton);
+    // cardBodyDiv.append(cardBodyButton);
 
     // append colDiv;
-    card123123.append(colDiv);
+    return colDiv;
   }
 
-  //
 });
