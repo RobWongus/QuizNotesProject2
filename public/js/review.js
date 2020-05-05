4780f3f8c5254ec8ac47baf0c3e55586b6f452dc
Alex Taylor <altays633@gmail.com>
2020-05-04 20:42:08 -0400

    adding psuedocoded logic for rendering cards from database based on selected category

--- public/js/review.js
+++ public/js/review.js
@@ -1,109 +1,123 @@
-  $(document).ready(function(){
-    // new category variables
-    let categoryId;
-    let categorySelect = $("#category")
-    let categoryNew = $("#newCategory")
+$(document).ready(function(){
+  // new category variables
+  let categoryId;
+  let categorySelect = $("#category");
+  let selectedOption;
+  let card123123 = $("#cards")
 
-    // new card variables
-    let questionInput = $("#questionInput");
-    let answerInput = $("#answerInput");
-    let cardButton = $("#newCardButton");
-    let newCard = $("#newCardForm")
-    // if(url.indexOf("?card_id=")!==1){
-    //     cardId = url.split("=")[1];
-    //     getCardData(cardId, "card");
-    // }
-    // else if(url.indexOf("?category_id=")!== -1){
-    //     categoryId = url.split("=")[1];
-    // }
+  // on submit for category form, run newCategorySUbmit
+  // $(document).on("submit", "#category-form", newCategorySubmit);
+  
+  getCategories();
+  // getCards();
 
-    $(document).on("submit", "#category-form", newCategorySubmit);
+  // createCard();
 
-    // new card submit button maybe?
-    $(newCard).on("submit", submitNewCard);
+  // calling categories for dropdowns
+  function getCategories(){
+      $.get("/api/categories", renderCategories)
+  }
+
+  //talks to db
+  function renderCategories(data){
+      // what does this do?
+      if(!data.length){
+          window.location.href = "/create"
+      }
+
+      // otherwise, create an empty array and then push the categories to it
+      // then, empty the category select element
+      // then, append the rows to add
+      // then, set the value of those options to the categoryId
+
+      let rowsToAdd = [];
+      for(let i = 0; i < data.length; i++){
+          rowsToAdd.push(createCategoryRow(data[i]));
+      }
+      categorySelect.empty();
+      categorySelect.append(rowsToAdd);
+      categorySelect.val(categoryId);
+  }
+
+  //creates category rows
+  function createCategoryRow(category){
+    // create an option
+    // target the value, set that to the id of the category
+    // set the text to the name of the category
+    // return the option
+
+    let options = $("<option>");
+    options.attr("value", category.id);
+    options.text(category.name);
+    return options;
+  }
+
+  // runs getCards api call
+  function getCards(category){
+    $.get("/api/cards/:id", renderCards);
+  }
+
+  // get selected Category when there is a change in category
+  $("select.form-control").change(function(){
+    selectedOption = $(this).children("option:selected").val();
+    console.log(selectedOption);
+  });
+  
+  //talk to dB to get cards from the database with the specific category
+  function renderCards(data){
+    // what does this do?
+    if(!data.length){
+        window.location.href = "/review"
+    }
+
+    // otherwise, create an empty object and then loop through the cards, then push all of the cards from the dB to that
+    // then, empty the category select element
+    // then, append the cards
+    // then, set the value of those options to the question, answer, and category ID
+    console.log(selectedOption)
+    createCard();
+  }
+
+  //creates category rows
+  function createCard(){
+    // create a series of nested divs
+      // col div
+      // card div
+      // card body div
+      // h5 element
+      // p element
+      // button element
+   
+    let colDiv = $("<div>");
+    let cardDiv = $("<div>");
+    let cardBodyDiv = $("<div>");
+    let cardBodyHeading = $("<h5>");
+    let cardBodyPara = $("<p>")
+    let cardBodyButton = $("<button>");
+
+    // from create
+    let options = $("<option>");
+    options.attr("value", category.id);
+    options.text(category.name);
+
+    // add classes, text
+      //col div - add col class and sizing
+      //card div - add card body class
+      //h5 - add card-title class, name text from dB
+      // p - add card-text, question text from db
+      // button - add button type, class, data toggle, data-target, data-type, and text
     
-    
-    getCategories();
-// calling categories for dropdowns
-    function getCategories(){
-        $.get("/api/categories", renderCategories)
+    // append divs and elements together
 
-    }
+    colDiv.append(cardDiv);
+    cardDiv.append(cardBodyDiv);
+    cardBodyDiv.append(cardBodyHeading);
+    cardBodyDiv.append(cardBodyPara);
+    cardBodyDiv.append(cardBodyButton);
 
-//talks to db
-    function renderCategories(data){
-        if(!data.length){
-            window.location.href = "/create"
-        }
-        let rowsToAdd = [];
-        for(let i = 0; i < data.length; i++){
-            rowsToAdd.push(createCategoryRow(data[i]));
-        }
-        categorySelect.empty();
-        // console.log(rowsToAdd);
-        // console.log(categorySelect);
-        categorySelect.append(rowsToAdd);
-        categorySelect.val(categoryId);
-        
-    }
+    // append colDiv;
+    card123123.append(colDiv);
+  }
 
-    function createCategoryRow(category){
-        let options = $("<option>");
-        options.attr("value", category.id);
-        options.text(category.name);
-        return options;
-    }
-
-    function newCategorySubmit(event){
-        event.preventDefault();
-        if(!categoryNew){
-            return
-        }
-
-        updateCategory({
-            name: categoryNew
-            .val()
-            .trim()
-        });
-    }
-
-    function updateCategory(categoryData){
-        console.log(categoryData);
-        $.post("/api/categories", categoryData)
-        .then(getCategories);
-    };
-
-// creating new card
-    
-    function submitNewCard(){
-        // event.preventDefault();
-        if(!questionInput.val().trim() || !answerInput.val().trim() || categorySelect.val()){
-            return;
-        }
-
-        let newCard = {
-            question: questionInput
-                .val()
-                .trim(),
-            answer: answerInput
-                .val()
-                .trim(),
-            CategoryId: categorySelect.val()
-        };
-        console.log(newCard);
-
-        submitCard(newCard);
-    };
-
-    function submitCard(card){
-        console.log(card);
-        $.post("/api/cards", card)
-        .then(getCategories)
-    };
-
-    // $(".create-card").click(function(event){
-    //     event.preventDefault();
-    //     submitNewCard();
-    // })
-
+  //
 });
