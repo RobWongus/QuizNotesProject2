ad7bcc8ace07889d22c790b74e549543ff4a2ac8
Nathan Long <nrlong13@gmail.com>
2020-05-06 01:20:09 -0400

    updated scripts

--- public/js/create.js
+++ public/js/create.js
@@ -1,41 +1,9 @@
 $(document).ready(function () {
     // new category variables
-    // let categoryId;
-    // let categorySelect = $("#category")
+
     let categoryNew = $("#newCategory")
 
     $(document).on("submit", "#category-form", newCategorySubmit);
-
-    // getCategories();
-    // // calling categories for dropdowns
-    // function getCategories() {
-    //     $.get("/api/categories", renderCategories)
-
-    // };
-
-    // //talks to db
-    // function renderCategories(data) {
-    //     if (!data.length) {
-    //         window.location.href = "/create"
-    //     }
-    //     let rowsToAdd = [];
-    //     for (let i = 0; i < data.length; i++) {
-    //         rowsToAdd.push(createCategoryRow(data[i]));
-    //     }
-    //     categorySelect.empty();
-    //     // console.log(rowsToAdd);
-    //     // console.log(categorySelect);
-    //     categorySelect.append(rowsToAdd);
-    //     categorySelect.val(categoryId);
-
-    // }
-
-    // function createCategoryRow(category) {
-    //     let options = $("<option>");
-    //     options.attr("value", category.id);
-    //     options.text(category.name);
-    //     return options;
-    // }
 
     function newCategorySubmit(event) {
         event.preventDefault();
@@ -49,9 +17,10 @@ $(document).ready(function () {
                 .trim()
         });
     }
+
     function updateCategory(categoryData) {
         console.log(categoryData);
-        $.post("/api/categories", categoryData, function(){
+        $.post("/api/categories", categoryData, function () {
             window.location.href = "/card"
         })
     };
